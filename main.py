741ae52de3a24ae2606d4ed1ec414cd5db4e2876
Daris <70913837+DarisBerzins@users.noreply.github.com>
2024-05-06 19:49:48 +0200

    tiled background

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame as pg
 
 pg.init()
+clock = pg.time.Clock()
+
+pg.display.set_caption("Balls")
+
 t0 = 0.001 * pg.time.get_ticks()
 
 maxdt = 0.5
@@ -12,45 +16,54 @@ screen = pg.display.set_mode(reso)
 
 velocity = 0
 acceleration = 0
-position = xmax//2
+position = 0
+position2 = xmax
 
 background = pg.image.load("assets/test-image.png")
 bgRect = background.get_rect()
+bgRect2 = background.get_rect()
 
 player = pg.Rect(100, ymax//2, 50, 50)
 
 font = pg.font.SysFont(None, 24)
-
 
 running = True
 while running:
     t = 0.001 * pg.time.get_ticks()
     dt = min(t-t0, maxdt)
     if dt > 0.0:
+        clock.tick()
+
         t0 = t
 
-        fpsImage = font.render(str(1/dt), True, (255, 255, 255))
-
+        fpsImage = font.render(str(int(clock.get_fps())), True, (255, 255, 255))
 
         pg.event.pump()
         keys = pg.key.get_pressed()
         if keys[pg.K_RIGHT]:
-            velocity += 10
-        if keys[pg.K_LEFT]:
             velocity += -10
+        if keys[pg.K_LEFT]:
+            velocity += 10
         if keys[pg.K_ESCAPE]:
             running = False
 
         velocity = velocity + acceleration * dt
         position = position + velocity * dt
 
-        bgRect.centerx = position
+        position2 = position + xmax
+        bgRect.left = position
+        bgRect2.left = position2
+
+        if position > xmax:
+            position = 0
+        elif position < -xmax:
+            position = 0
         
         screen.blit(background, bgRect)
+        screen.blit(background, bgRect2)
         screen.blit(fpsImage, (20, 20))
         pg.draw.rect(screen, (128, 0, 255), player)
 
         pg.display.flip()
-
 
 pg.quit()
